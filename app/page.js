c214fb637530a13c49310166be8086a89b34cdd7
mianne <michelle.mendoza.other@gmail.com>
2024-07-31 23:34:36 +0800

    feat(*): stars background | resume download | removed dark/light mode

--- app/page.js
+++ app/page.js
@@ -15,17 +15,28 @@ import web3 from "../public/web3.png";
 import web4 from "../public/web4.png";
 import web5 from "../public/web5.png";
 import web6 from "../public/web6.png";
+import lm from "../public/lm.png";
+import wink from "../public/wink.png";
+
+import Starfield from "react-starfield";
 import { useState } from "react";
 export default function Home() {
   const [darkMode, setIsDarkMode] = useState(false);
   return (
-    <div className={darkMode ? "dark" : ""}>
-      <main className="bg-white px-10 md:px-20 lg:px-40  dark:bg-slate-800">
+    <div className={"dark"}>
+      <Starfield
+        starCount={1000}
+        starColor={[255, 255, 255]}
+        speedFactor={0.05}
+        backgroundColor="black"
+      />
+      <div className=" px-10 md:px-20 lg:px-40  ">
         <section className="min-h-screen">
           <nav className="p-10 mb-12 flex justify-between">
-            <h1 className="text-xl  font-burtons dark:text-white">
+            {/* <h1 className="text-xl  font-burtons dark:text-white">
               developedbymich
-            </h1>
+            </h1> */}
+            <Image alt="Portfolio Icon" src={lm} width={55} height={55} />
             <ul className="flex items-center dark:text-white">
               <li className="cursor-pointer text-2xl">
                 <BsFillMoonStarsFill onClick={() => setIsDarkMode(!darkMode)} />
@@ -33,39 +44,47 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                  href="#"
+                  href="/resume.pdf"
+                  download="resume"
                 >
                   Resume
                 </a>
               </li>
             </ul>
           </nav>
-          <div className="text-center p-10">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
-              Michelle Anne F. Mendoza
-            </h2>
-            <h3 className="text-2xl py-2 md:text-3xl dark:text-white">
-              Software Engineer and Full Stack developer
-            </h3>
-            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-white">
-              I'm a software engineer who loves to create amazing apps that
-              helps people. Join me down below and let's get something started.
-            </p>
-          </div>
-          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-white">
-            <a
-              href="https://www.linkedin.com/in/michelle-anne-m-03b95a232"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <AiFillLinkedin />
-            </a>
-            {/* <AiFillGoogleCircle /> */}
-            <SiGmail />
-            <FaDiscord />
-          </div>
-          <div className="relative bg-gradient-to-b from-teal-500 overflow-hidden rounded-full w-80 h-80 mt-20 mx-auto md:h-96 md:w-96">
-            <Image alt=""src={pf2} layout="fill" objectFit="cover" />
+          <div className="border border-amber-900 border-t-1">
+            <div className="text-center p-10">
+              <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
+                Michelle Anne F. Mendoza
+              </h2>
+              <h3 className="text-2xl py-2 md:text-3xl dark:text-white">
+                Software Engineer and Full Stack developer
+              </h3>
+              <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-white">
+                {` I'm a software engineer who loves to create amazing apps that
+              helps people. Join me down below and let's get something started.`}
+              </p>
+            </div>
+            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-white">
+              <a
+                href="https://www.linkedin.com/in/michelle-anne-m-03b95a232"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <AiFillLinkedin />
+              </a>
+              {/* <AiFillGoogleCircle /> */}
+              <SiGmail />
+              <FaDiscord />
+            </div>
+            <div className="relative bg-gradient-to-b from-teal-500 overflow-hidden rounded-full w-80 h-80 mt-20 mx-auto md:h-96 md:w-96">
+              <Image
+                alt="Bitmoji image of the portfolio owner"
+                src={wink}
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
           </div>
         </section>
         {/* HEADER DONE */}
@@ -88,7 +107,13 @@ export default function Home() {
           </div>
           <div className="lg:flex gap-10 dark:text-white">
             <div className="text-center shadow-lg p-10 rounded-xl my-10">
-              <Image alt=""src={design} width={100} height={100} className="inline" />
+              <Image
+                alt=""
+                src={design}
+                width={100}
+                height={100}
+                className="inline"
+              />
               <h3 className="text-lg font-medium pt-8 pb-2">
                 Beautiful Designs
               </h3>
@@ -101,7 +126,13 @@ export default function Home() {
               <p className="text-gray-800 py-1 dark:text-white">Illustrator</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10">
-              <Image alt="" src={code} width={100} height={100} className="inline" />
+              <Image
+                alt=""
+                src={code}
+                width={100}
+                height={100}
+                className="inline"
+              />
               <h3 className="text-lg font-medium pt-8 pb-2">Clean Code</h3>
               <p className="py-2">
                 Creating elegant designs suited for your needs
@@ -214,7 +245,7 @@ export default function Home() {
             </div>
           </div>
         </section>
-      </main>
+      </div>
     </div>
   );
 }
